fd6027035d57f68bcb0bd5ecdac7ae1ed9227a89
Mateusz Los <mateusz.los@cognifide.com>
2014-11-18 15:04:04 +0100

    v0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robertomoutinho@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 depends 'ark'
 depends 'java', '= 1.28.0'
